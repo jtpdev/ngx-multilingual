5897937ec41424e87058964eea696cd7cf2d9bdf
Jimmy Porto <ireturnerd@gmail.com>
2018-05-25 13:01:25 -0300

    Updated tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MultilingualService } from './multilingual/multilingual.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  
+  constructor(
+    public multilingual: MultilingualService
+  ) {
+    multilingual.path('/multilingual');
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MultilingualService } from './multilingual/multilingual.service';
 
 
 @NgModule({
@@ -12,7 +13,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [MultilingualService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
